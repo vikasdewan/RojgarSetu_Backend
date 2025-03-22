3185f253e7abc782b86fe03bc761df5b91deba04
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2025-03-22 20:09:38 +0530

    database connection at app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import pkg from "body-parser";
+import connectDB from "./db";
 
 const { urlencoded } = pkg;
 
@@ -10,12 +11,29 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 const corsOptions = {
+    allowHeaders:["Content-Type"],
     origin: 'http://localhost:5173',
+    methods:"GET,PUT,POST,DELETE,PATCH",
     credentials: true,
 };
+
 app.use(cors(corsOptions));
+const port = process.env.PORT || 3000;
+//database connection 
+connectDB()
+.then(()=>{
+    app.listen(port,()=>{
+       console.log(`server is running at http://localhost:${port}`);
+    })
+})
+.catch((err)=>{
+    console.log("Mongo DB connection error :: ",err);
+    
+})
+
 
 app.get("/", (req, res) => {
     return res.status(200).json({
