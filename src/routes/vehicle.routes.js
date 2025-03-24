0408427cd7ceaab1442124ca95d8bb5d258fe03b
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2025-03-24 17:56:33 +0530

    vehicle routes

--- src/routes/vehicle.routes.js
+++ src/routes/vehicle.routes.js
@@ -1,5 +1,26 @@
-import { Router } from "express";
+import express from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import {
+  createVehicleListing,
+  updateVehicleListing,
+  deleteVehicleListing,
+  getVehicleDetails,
+  listVehicles,
+  searchVehicles
+} from "../controllers/vehicle.controller.js";
+import { checkRole } from "../middlewares/role.middleware.js";
 
-const router = Router();
+const router = express.Router();
+
+router.route("/")
+  .post(verifyJWT, checkRole(['VehicleOwner']), createVehicleListing)
+  .get(listVehicles);
+
+router.route("/:id")
+  .get(getVehicleDetails)
+  .patch(verifyJWT, checkRole(['VehicleOwner']), updateVehicleListing)
+  .delete(verifyJWT, checkRole(['VehicleOwner']), deleteVehicleListing);
+
+router.route("/search").get(searchVehicles);
 
 export default router;
