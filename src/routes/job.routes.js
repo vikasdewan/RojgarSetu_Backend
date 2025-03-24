67322b50a51ad1ca6ac9cbc629b4d0f28f9b0785
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2025-03-24 17:54:15 +0530

    job routes

--- src/routes/job.routes.js
+++ src/routes/job.routes.js
@@ -1,5 +1,29 @@
-import { Router } from "express";
+import express from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import {
+  createJob,
+  updateJob,
+  deleteJob,
+  getJobDetails,
+  listJobs,
+  searchJobs,
+  getJobApplications
+} from "../controllers/job.controller.js";
+import { checkRole } from "../middlewares/role.middleware.js";
 
-const router = Router();
+const router = express.Router();
+
+// Job CRUD Operations
+router.route("/")
+  .post(verifyJWT, checkRole(['Employer']), createJob)
+  .get(listJobs);
+
+router.route("/:id")
+  .get(getJobDetails)
+  .patch(verifyJWT, checkRole(['Employer']), updateJob)
+  .delete(verifyJWT, checkRole(['Employer']), deleteJob);
+
+router.route("/search").get(searchJobs);
+router.route("/:id/applications").get(verifyJWT, checkRole(['Employer']), getJobApplications);
 
 export default router;
